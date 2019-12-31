b082b9803410df593b6aa1cc1307ddc375309b95
Ankit <panditankit1995@gmail.com>
2019-12-31 19:14:39 +0530

    vuetify/i18n installed

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import Vue from 'vue';
 import App from './App.vue';
-import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify/vuetify';
+import i18n from './plugins/i18n/i18n';
+
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   router,
   store,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
